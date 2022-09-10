994dc50f4fdd739d62a0792fdb85901c26d0607a
Guillermo <guillermo.arena@gmail.com>
2022-09-10 19:43:33 -0300

    educacionts

--- proyectoIntegrador/src/app/app.module.ts
+++ proyectoIntegrador/src/app/app.module.ts
@@ -9,17 +9,21 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
-import { EducacionComponent } from './components/educacion/educacion.component';
-import { NgCircleProgressModule } from 'ng-circle-progress';
-import { HysComponent } from './components/hys/hys.component';
+
+
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
-import { NewExperienciaComponent } from './components/experiencia/new-experiencia/new-experiencia.component';
+
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HysComponent } from './components/hys/hys.component';
 
 
 @NgModule({
@@ -45,7 +49,7 @@ import { EditExperienciaComponent } from './components/experiencia/edit-experien
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    NgCircleProgressModule.forRoot({}),
+    NgCircleProgressModule,
     HttpClientModule
   ],
   providers: [
